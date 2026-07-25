f2f568dcec0be8ea8d462fc0797b78e46844f061
Ankit Prajapati <ankitprajapati@mac.home>
2026-07-25 00:03:46 -0400

    add backend url

--- src/app/environment/environment.ts
+++ src/app/environment/environment.ts
@@ -1,4 +1,5 @@
 export const env_variable = {
-    SERVER_URL: 'http://127.0.0.1:8000',
+    // SERVER_URL: 'http://127.0.0.1:8000',
+    SERVER_URL: 'https://stamp-network-backend-1030483456536.northamerica-northeast2.run.app',
     REWARD: '10%'
 }
